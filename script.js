2eac1fe5391cfc647d52384dd4dd64d434c08711
Makarios-Tech101 <ogodogublessing1@gmail.com>
2024-02-12 11:41:52 +0100

    currentIndex executed

--- script.js
+++ script.js
@@ -1,26 +1,46 @@
-async function generateImg() {
+
+let currentIndex = 0; // Initialize a variable to keep track of the current index
+
+async function fetchUser() {
     
     try{
-    //   const elButton = document.getElementById("btn-image");
-    //   const response = await fetch(`https://jsonplaceholder.typicode.com/photos/20`);
-    const randomId = Math.floor(Math.random() * 100) + 1; // Generate a random ID between 1 and 100
-    const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${randomId}`);
-      if (!response.ok) {
-        throw new Error("Could not fetch resources")
-      }
+     const response = await fetch('https://jsonplaceholder.typicode.com/todos/');
+        if (!response.ok) {
+          throw new Error("Could not fetch resources")
+        }
 
       const data = await response.json();
-    //   console.log(data[3]);
-      const generatedImage = data.url;
+      console.log("MY DATA",data);
 
-      const imgElement = document.getElementById("generatedImage");
+      const Element = document.getElementById("paragraph");
 
-      imgElement.src = generatedImage;
+    
+      // Check if currentIndex exceeds the length of the data array
+      if (currentIndex >= data.length) {
+        currentIndex = 0; // Reset the index to display titles from the beginning
+      }
 
-      imgElement.style.display = "inline";
-      imgElement.style.maxWidth = "300px";
-      imgElement.style.marginTop = "10px";
-      // imgElement.style.margin = "0px auto";
+    // Display the title at the current index
+      // Element.innerHTML = data[currentIndex].title;
+      const currentTodo = data[currentIndex];
+
+        // Display all the information of the current todo
+        Element.innerHTML = `
+            <p>User ID: ${currentTodo.userId}</p>
+            <p>ID: ${currentTodo.id}</p>
+            <p>Title: ${currentTodo.title}</p>
+            <p>Completed: ${currentTodo.completed}</p>
+        `;
+
+      currentIndex++; // Increment the index for the next click
+      
+        //  for (let todo of data){
+
+        //   Element.innerHTML += `${todo.title } <br>` ;
+        //  }
+      // Element.textContent = paragraph;
+
+      
     }
 
     catch(error) {
